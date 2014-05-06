8391084bec8957467f28f70fc616ab0000e4342b
MadeInOuweland <info@loekvandenouweland.com>
2014-05-06 14:41:42 +0200

    * go

---
+++ RxSearch/Api.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace RxSearch
+{
+    public class Api
+    {
+        public IObservable<List<string>> Search(string filter)
+        {
+            var list = new List<string> { "Vera", "Chuck", "Dave", "John", "Paul", "Ringo", "George" };
+            var filteredList = list.Where(x => x.ToLower().Contains(filter.ToLower()));
+            return Observable.Return(filteredList.ToList());
+        }
+    }
+}
---
+++ RxSearch/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using Windows.UI.Xaml.Controls;
+
+namespace RxSearch
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var api = new Api();
+
+            var textchanges = Observable.FromEventPattern<TextChangedEventHandler, TextChangedEventArgs>(
+                h => textbox.TextChanged += h,
+                h => textbox.TextChanged -= h
+                ).Select(x => ((TextBox)x.Sender).Text);
+
+            textchanges
+                .Throttle(TimeSpan.FromMilliseconds(300)) // result on threadpool 
+                .SelectMany(api.Search)
+                .ObserveOnDispatcher() // send back to dispatcher
+                .Subscribe(OnSearchResult);
+
+            api.Search("").Subscribe(OnSearchResult);
+        }
+
+        private void OnSearchResult(List<string> list)
+        {
+            listbox.ItemsSource = list;
+        }
+    }
+}
